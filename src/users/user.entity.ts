66f68d4d59a12e51fb6380671eab2216614df896
Anastasiya <sulimova.a.m@yandex.ru>
2024-05-28 15:13:07 +0300

    create user.entity and user.module

---
+++ src/users/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, length: 64 })
+  username: string;
+
+  @Column({ default: 'Пока ничего не рассказал о себе', length: 200 })
+  about: string;
+
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  avatar: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
