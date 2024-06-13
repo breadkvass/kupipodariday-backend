dfdd846fa37e3a537df095e53752a69b6b5fc29c
Anastasiya Sulimova <breadkvass@gmail.com>
2024-06-13 21:34:47 +0300

    add create, update, remove and copy functions to wishes.controller

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { WishesService } from './wishes.service';
+import { CreateWishDto } from './dto/create-wish.dto';
+import { UpdateWishDto } from './dto/update-wish.dto';
 
 @Controller('wishes')
 export class WishesController {
   constructor(private readonly wishesService: WishesService) {}
 
-  @Get('last')
-  findLast() {
-    return this.wishesService.findAll({
-      order: { createdAt: 'DESC' },
-      take: 40,
-    });
+  // TODO: добавить @UseGuards
+  @Post()
+  create(@Request() req, @Body() createWishDto: CreateWishDto) {
+    return this.wishesService.create(req.user.userId, createWishDto);
   }
 
   @Get('top')
@@ -21,14 +33,72 @@ export class WishesController {
     });
   }
 
+  @Get('last')
+  findLast() {
+    return this.wishesService.findAll({
+      order: { createdAt: 'DESC' },
+      take: 40,
+    });
+  }
+
   @Get(':id')
   async findOne(@Param('id') id: string) {
+    const wish = await this.wishesService.findOne(+id);
+    if (!wish) {
+      throw new NotFoundException('Желание не найдено');
+    }
+    return wish;
+  }
+
+  // TODO: добавить @UseGuards
+  @Patch(':id')
+  async update(@Request() req, @Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
     const wish = await this.wishesService.findOne(+id);
 
     if (!wish) {
       throw new NotFoundException('Желание не найдено');
     }
 
-    return wish;
+    if (wish.owner.id !== req.user.userId) {
+      throw new ForbiddenException('У Вас нет доступа для редактирования этого желания');
+    }
+
+    return this.wishesService.update(+id, updateWishDto);
+  }
+
+  // TODO: добавить @UseGuards
+  @Delete(':id')
+  async remove(@Request() req, @Param('id') id: string) {
+    const wish = await this.wishesService.findOne(+id);
+    
+    if (!wish) {
+      throw new NotFoundException('Желание не найдено');
+    }
+
+    if (wish.owner.id !== req.user.userId) {
+      throw new ForbiddenException('You are not allowed to delete this wish');
+    }
+
+    return this.wishesService.remove(+id);
+  }
+
+  // TODO: добавить @UseGuards
+  @Post(':id/copy')
+  async copy(@Request() req, @Param('id') id: string) {
+    const wish = await this.wishesService.findOne(+id);
+
+    if (!wish) {
+      throw new NotFoundException('Желание не найдено');
+    }
+
+    delete wish.id;
+    const copiedWish = await this.wishesService.create(req.user.userId, {
+      ...wish,
+      copied: 0,
+    });
+
+    await this.wishesService.incrementCopied(+id);
+
+    return copiedWish;
   }
 }
